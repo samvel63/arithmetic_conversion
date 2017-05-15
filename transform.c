7e73671bd13fbccb39cb322517b47756ac5748ac
samvel63 <mhitaryan.1998@mail.ru>
2017-05-16 01:40:36 +0300

    Убрана утечка

--- transform.c
+++ transform.c
@@ -13,6 +13,9 @@ uint32_t trees_is_equal(Tree t1, Tree t2)
     if (!t1 || !t2)
         return 0;
 
+    if (t1->node.type == OPERATOR && t1->node.type == t2->node.type && (t1->node.data.operator == '/' || t1->node.data.operator == '-'))
+        return  t1->node.data.operator == t2->node.data.operator && trees_is_equal(t1->right, t2->right) && trees_is_equal(t1->left, t2->left);
+
     return ((t1->node.type == INTEGER && t1->node.type == t2->node.type && t1->node.data.value_int == t2->node.data.value_int) ||
            (t1->node.type == VARIABLE && t1->node.type == t2->node.type && t1->node.data.variable == t2->node.data.variable)  ||
            (t1->node.type == OPERATOR && t1->node.type == t2->node.type && t1->node.data.operator == t2->node.data.operator)) &&
@@ -32,8 +35,12 @@ void tree_simplify(Tree t)
 			if (t->right->node.data.value_int == 1) {
 				t->node.type = t->left->node.type;
 				t->node.data.value_int = t->left->node.data.value_int;
+                Tree tmp_1 = t->left->right;
+                Tree tmp_2 = t->left->left;
 				tree_destroy(&t->right);
-				tree_destroy(&t->left);
+				free(t->left);
+                t->right = tmp_1;
+                t->left = tmp_2;
 			}
 		}
 	}
@@ -53,10 +60,14 @@ void tree_simplify(Tree t)
 					tree_destroy(&t->right);
                     tree_destroy(&t->left);
 				} else if (t->right->node.type == OPERATOR) {
-                    t->node.data.operator = t->right->node.data.operator;
+                   // t->node.data.operator = t->right->node.data.operator;
+                    char operator = t->right->node.data.operator;
                     Tree tmp_1 = t->right->right;
                     Tree tmp_2 = t->right->left;
-                    t->left = NULL;
+                    tree_destroy(&t->left);
+                    free(t->right);
+                    t->node.type = OPERATOR;
+                    t->node.data.operator = operator;
                     t->left = tmp_1;
                     t->right = tmp_2;
 				}
@@ -74,17 +85,21 @@ void tree_simplify(Tree t)
 					tree_destroy(&t->left);
                     tree_destroy(&t->right);
 				} else if (t->left->node.type == OPERATOR) {
-                    t->node.data.operator = t->left->node.data.operator;
+                    //t->node.data.operator = t->left->node.data.operator;
+                    char operator = t->left->node.data.operator;
                     Tree tmp_1 = t->left->right;
                     Tree tmp_2 = t->left->left;
-                    t->right = NULL;
+                    tree_destroy(&t->right);
+                    free(t->left);
+                    t->node.type = OPERATOR;
+                    t->node.data.operator = operator;
                     t->right = tmp_1;
                     t->left = tmp_2;
 				}
 			}
 
 		}
-	}
+	} // (a+a+a)/(a+a+a)+(a*v+i*u*v)/(v*a+i*v)+(5*a/(5*b))
 
 }
 
@@ -151,6 +166,9 @@ uint32_t is_constant_in_tree(Tree t, uint32_t n)
     if (!t)
         return 0;
 
+
+
+
     if (t->node.data.value_int == n) {
         return 1;
     }
@@ -183,33 +201,6 @@ void transform(Tree t1, Tree t2)
 {
 	if (!t1)
 		return;
-
-    if (t1->node.type == OPERATOR && t1->node.data.operator == '*') {
-        if (is_common(t1->left, t2)) {
-            transform(t1->left, t2);
-            return;
-        } if (is_common(t1->right, t2)) {
-            transform(t1->right, t2);
-            return;
-        }
-
-    }
-
-    if (t2->node.type == OPERATOR && t2->node.data.operator == '*') {
-        /*if (is_common(t2, t1)) {
-            transform(t2->left, t1);
-            transform(t2->right, t1);
-            return;
-        }*/
-
-        if (is_common(t2->left, t1)) {
-            transform(t2->left, t1);
-            return;
-        } if (is_common(t2->right, t1)) {
-            transform(t2->right, t1);
-            return;
-        }
-    }
 
 
     if (t1->node.type == VARIABLE) {
@@ -248,6 +239,26 @@ void transform(Tree t1, Tree t2)
         }
     }
 
+    if (t1->node.type == OPERATOR && t1->node.data.operator == '*') {
+        if (is_common(t1->left, t2))
+            transform(t1->left, t2);
+
+        if (is_common(t1->right, t2))
+            transform(t1->right, t2);
+
+        return ;
+    }
+
+    if (t2->node.type == OPERATOR && t2->node.data.operator == '*') {
+        if (is_common(t2->left, t1))
+            transform(t2->left, t1);
+
+        if (is_common(t2->right, t1))
+            transform(t2->right, t1);
+
+        return;
+    }
+    
     if (t1->node.type == OPERATOR && (t1->node.data.operator == '-' || t1->node.data.operator == '+')) {
         if (is_common(t1->left, t2) && is_common(t1->right, t2)) {
             transform(t1->left, t1->right);
@@ -255,7 +266,6 @@ void transform(Tree t1, Tree t2)
             return;   
         }
     }
-
 }
 
 void find_det(Tree tree)
